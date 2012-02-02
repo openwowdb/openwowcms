3725d1c85b08520c628a4fa5272edd0120348890
Swiftsmoke <dibble1989@hotmail.co.uk>
2012-02-03 01:36:24 +0800

    Shoutbox changes
    Seperated the javascript from the php by placing
    	all the functions into Engine -> JS -> Shoutbox.js
    Removed the need for php callback to update times (now handled via js)
    Updated shoutbox self installation to add itself to the module_userpanel exclude list
    Now only 2 variables that need adjusting (refresh time & idle time)
    Added a message and link if the shoutbox variables haven't been installed yet

--- engine/modules/shoutbox.php
+++ engine/modules/shoutbox.php
@@ -19,11 +19,18 @@ if (isset($user))
 	**/
 	if (!isset($proccess))
 	{
+		$exclude_modulenames=explode('|',$config['module_userpanel']);
+		if (array_search("shoutbox.php", $exclude_modulenames) == null)
+		{
+			array_push($exclude_modulenames, 'shoutbox.php');
+			$config['module_userpanel'] = implode('|', $exclude_modulenames);
+		}
 		if($Html->moduleinstall('shoutbox',
-			array('shoutbox_refresh_time','shoutbox_update_old_time','shoutbox_idle_time'),
-			array('30','60','120'),
-			array('How often should the shoutbox update (in seconds).','How often should the shoutbox old post time be updated (in seconds, 0 = off)','When should shoutbox stop updating if player has not said anything (in seconds.. 0 = off)'),
-			array('CREATE TABLE IF NOT EXISTS '.$config['engine_web_db'].'.mod_shoutbox (`id` bigint(20) NOT NULL AUTO_INCREMENT, `poster` varchar(255) COLLATE latin1_general_ci NOT NULL, `message` text COLLATE latin1_general_ci NOT NULL,`timepost` varchar(100) COLLATE latin1_general_ci NOT NULL,PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;') // Sql
+			array('shoutbox_refresh_time','shoutbox_idle_time'),
+			array('30','120'),
+			array('How often should the shoutbox update (in seconds)','When should shoutbox stop updating  (in seconds.. 0 = off)'),
+			array('DROP TABLE '.$config['engine_web_db'].'.mod_shoutbox;', 'CREATE TABLE IF NOT EXISTS '.$config['engine_web_db'].'.mod_shoutbox (`id` bigint(20) NOT NULL AUTO_INCREMENT, `poster` varchar(255) COLLATE latin1_general_ci NOT NULL, `message` text COLLATE latin1_general_ci NOT NULL,`timepost` varchar(100) COLLATE latin1_general_ci NOT NULL,PRIMARY KEY (`id`)) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;',
+				 "UPDATE ".$config['engine_web_db'].".wwc2_config SET conf_value='".$db->escape($config['module_userpanel'])."' WHERE conf_name='module_userpanel'")
 			))
 			return;
 	}
@@ -58,50 +65,49 @@ require(PATHROOT . "engine/init.php");
 require(PATHROOT . "engine/func/parser.php");
 require(PATHROOT . "engine/func/nicetime.php");
 
-if (isset($_POST['message']))
+if (!function_exists("echo_shout"))
 {
-	if($user->logged_in && trim($_POST['message']) <> '')
-		$db->query("INSERT INTO ".$config['engine_web_db'].".mod_shoutbox (poster,message,timepost) VALUES ('".$db->escape($user->username)."','".$db->escape($_POST['message'])."','".date("U")."')") or die(mysql_error());
-}
-
-if (isset($_POST['dotimeupdate']) && isset($_POST['data']))
-{
-	$items = explode(";", $_POST['data']);
-	echo "<script type='text/javascript'>";
-	if (count($items) > 1)
+	function echo_shout($arr)
 	{
-		foreach ($items as $item)
+		global $user;
+		if (count($arr) >= 1)
 		{
-			$subitem = explode("|", $item);
-			if (count($subitem) == 2)
+			foreach ($arr as $shout)
 			{
-				echo "$('#".$subitem[0]."_update').html('(". nicetime($subitem[1]).")');";
+				echo '<div id="shoutmsg' . $shout['id'] . '">';
+				$userinfo=$user->getUserInfo($shout['poster']);
+				echo '<img src="'.$user->avatar($userinfo['avatar']).'" height="15px" width="15px" alt="avatar" /> ';
+				echo $shout['poster'].": ";
+				echo '<font color="#00FFFF">'.$shout['message'].'</font><br> <small class="comments_poster" id="time_'.$shout['id'].'_update">('. nicetime($shout['timepost']).')</small>';
+				echo '<var id="time_' . $shout['id'] . '" style="display:none;">'.$shout['timepost'].'</var>';
+				echo '</div>';
 			}
 		}
 	}
-	echo "</script>";
+}
+
+
+if (isset($_POST['message']))
+{
+	if($user->logged_in && trim($_POST['message']) <> '')
+	{
+		$username = $db->escape($user->username);
+		$message = $db->escape($_POST['message']);
+		$date = date("U");
+		$db->query("INSERT INTO ".$config['engine_web_db'].".mod_shoutbox (poster,message,timepost) VALUES ('".$username."','".$message."','".$date."')") or die(mysql_error());
+		$id = mysql_insert_id();
+		echo_shout(array(array('id'=>$id, 'poster'=>$username, 'message'=>$message, 'timepost'=>$date)));
+	}
 }
 
 if (isset($_POST['latest']))
 {
-	$shouts_sql = $db->query("SELECT * FROM ".$config['engine_web_db'].".mod_shoutbox WHERE id >= " .$db->escape($_POST['latest'])."  ORDER by id DESC") or die(mysql_error());
+	$shouts_sql = $db->query("SELECT * FROM ".$config['engine_web_db'].".mod_shoutbox WHERE id > " .$db->escape($_POST['latest'])."  ORDER by id DESC") or die(mysql_error());
 	$shouts = array();
 	while ($row = $db->fetch_array($shouts_sql))
 	{
 		array_push($shouts, $row);
 	}
-	if (count($shouts) >= 1)
-	{
-		foreach ($shouts as $shout)
-		{
-			echo '<div id="shoutmsg' . $shout['id'] . '">';
-			$userinfo=$user->getUserInfo($shout['poster']);
-			echo '<img src="'.$user->avatar($userinfo['avatar']).'" height="15px" width="15px" alt="avatar" /> ';
-			echo $shout['poster'].": ";
-			echo '<font color="#00FFFF">'.$shout['message'].'</font><br> <small class="comments_poster" id="time_'.$shout['id'].'_update">('. nicetime($shout['timepost']).')</small>';
-			echo '<var id="time_' . $shout['id'] . '" style="display:none;">'.$shout['timepost'].'</var>';
-			echo '</div>';
-		}
-	}
+	echo_shout($shouts);
 }
 ?>
