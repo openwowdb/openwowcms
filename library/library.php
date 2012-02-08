cc938b9b82681dd7df227310a0ea0083a66db107
Swiftsmoke <dibble1989@hotmail.co.uk>
2012-02-08 11:40:38 +0000

    Added an autoloader for classes in the library directory
    What this does is if a class hasn't been preloaded via an include/require, it will attempt to find the class in the library classes section.
    
    The filenaming is strict:
     - it must be lowercase
     - it must end with .php
     - the class must be the same name as the file (example new myclass would load myclass.php)
    
    Added an init() to library, this will be called on the load of the library class and it scans the database directory for support database types.
    To add a database the file must be database_xxx.php where xxx is the database type (example database_mysql.php)
    The class within the database must match the file name (class database_mysql extends database)
    
    Note: you can still use
    include "myclass.php";
    new myclass;
    
    To create a class pointer, this is just for the library specific classes

--- library/library.php
+++ library/library.php
@@ -1,57 +1,86 @@
 <?php
 /**
- * class library
- *
- * Description for class library
- *
- * @author:
+*   The autoload function is introduced in php v5, and is called whenever a new class is initiated.
+*
+* @param string Class Name
+* @return void
+* @access public
 */
-class library  {
-	static $supportdbs = array("mysql");
+function __autoload($className) {
+	$className = strtolower($className);
+	if (class_exists($className, false)) return;
+
+	$directories = array('library/classes/database/', 'library/classes/file/',
+	'library/classes/modules/', 'library/classes/security/');
+	foreach ($directories as $directory) {
+		$path = PATHROOT . $directory . $className . ".php";
+		if (file_exists($path)) {
+			include $path;
+			return;
+		}
+	}
+}
+
+/**
+* class library
+*
+* Description for class library
+*
+* @author:
+*/
+class library {
+	static $supportdbs = array();
+
+	static function init() {
+		// Autogenerate Database Types
+		$database_files = filehandler::getDir("library/classes/database", false, '/[^database_][a-z]\.php$/i');
+		foreach($database_files as $file) {
+			// Remove file extension (.PHP or .php)
+			$file = preg_replace("/.php/i", "", $file);
+			// Find database type (database_XXXXX.php)
+			$file = explode("_", $file);
+			array_push(self::$supportdbs, $file[1]);
+		}
+	}
 
 	/**
-	 * _htmlspecialchars
-	 *
-	 * @param string $string
-	 * @return string
-	 *
-	 */
-	static function _htmlspecialchars($string)
-	{
+	* _htmlspecialchars
+	*
+	* @param string $string
+	* @return string
+	*
+	*/
+	static function _htmlspecialchars($string) {
 		$string = preg_replace('/&(?!#[0-9]+;)/s', '&amp;', $string);
 		$string = str_replace(array('<', '>', '"'), array('&lt;', '&gt;', '&quot;'), $string);
 		return $string;
 	}
 
 	/**
-	 * supported_databases
-	 *
-	 * @param string $dbtype If not false then it will check its a valid database type
-	 * @return mixed Returns true or array(self::$supportdbs)
-	 *
-	 */
-	static function supported_databases($dbtype = false)
-	{
+	* supported_databases
+	*
+	* @param string $dbtype If not false then it will check its a valid database type
+	* @return mixed Returns true or array(self::$supportdbs)
+	*
+	*/
+	static function supported_databases($dbtype = false) {
 		if ($dbtype == false) return self::$supportdbs;
 		if (in_array($dbtype, self::$supportdbs)) return true;
 	}
 
 	/**
-	 * Creates a new database link object
-	 *
-	 * @param ref $dblink Database link variable
-	 * @param string $type Type of database connection (mysql, mssql, etc)
-	 *
-	 */
-	static function create_dblink(&$dblink, $type)
-	{
+	* Creates a new database link object
+	*
+	* @param ref $dblink Database link variable
+	* @param string $type Type of database connection (mysql, mssql, etc)
+	*
+	*/
+	static function create_dblink(&$dblink, $type) {
 		if ($type == "") return;
+		if (!self::supported_databases($type)) die(trigger_error("Unknown database type " . $type, E_ERROR));
 		$className = "database_$type";
-		if (!class_exists($className))
-		{
-			include "classes/database/$className.php";
-		}
 		$dblink = new $className;
 	}
 }
+library::init();
 ?>
